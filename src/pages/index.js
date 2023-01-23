a7d33f5f15d5093d40a12a8b4fb1d4320e701c7e
IXackerr <62063826+IXackerr@users.noreply.github.com>
2023-01-23 16:55:19 +0100

    1 binaries uploaded

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,51 +4,47 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
-
+import CodeBlock from '@theme/CodeBlock';
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-<header className={clsx('container', styles.heroBanner)}>
-            <div className="row padding-horiz--md">
-                <div className="col col--7">
-                    <div className={clsx(styles.relative, 'row')}>
-                        <div className="col">
-                            <h1 className={styles.tagline}>
-                                Crawlee is a web<br /> scraping and browser<br /> automation library
-                            </h1>
-                            <h1 className={styles.tagline}>
-                                Crawlee is a <span>web<br /> scraping</span> and <span>browser<br /> automation</span> library
-                            </h1>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <h2>It helps you build reliable crawlers. Fast.</h2>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <div className={styles.heroButtons}>
-                                <Link to="docs/introduction" className={styles.getStarted}>Get Started</Link>
-                                <iframe src="https://ghbtns.com/github-btn.html?user=apify&repo=crawlee&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className={clsx(styles.relative, 'col', 'col--5')}>
-                    <div className={styles.logoBlur}>
-                        <img src={require('../../static/img/logo-blur.png').default} className={clsx(styles.hideSmall)} />
-                    </div>
-                    <div className={styles.codeBlock}>
-                        <CodeBlock className="language-bash">
-                            npx crawlee create my-crawler
-                        </CodeBlock>
-                    </div>
-                </div>
+    <header className={clsx('container', styles.heroBanner)}>
+      <div className="row padding-horiz--md">
+        <div className="col col--7">
+          <div className={clsx(styles.relative, 'row')}>
+            <div className="col">
+              <h1 className={styles.tagline}>
+              Вы много чего не знаете о наших ботах
+              </h1>
             </div>
-        </header>
+          </div>
+          <div className="row">
+            <div className="col">
+              <h2>А эта wiki поможет вам разобраться</h2>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className={styles.heroButtons}>
+                <Link to="docs/intro" className={styles.getStarted}>
+                  Начнём
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={clsx(styles.relative, 'col', 'col--5')}>
+          <div className={styles.logoBlur}>
+            <img
+              src={require('../../static/img/logo-blur.png').default}
+              className={clsx(styles.hideSmall)}
+            />
+          </div>
+        </div>
+      </div>
+    </header>
   );
 }
 
@@ -57,9 +53,6 @@ export default function Home() {
   return (
     <Layout title={`Привет ${siteConfig.title}`}>
       <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
     </Layout>
   );
 }

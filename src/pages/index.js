88d6e7d543143a50ef118dce2dd3acc5e99ebca8
IXackerr <62063826+IXackerr@users.noreply.github.com>
2023-01-23 16:49:27 +0100

    1 binaries uploaded

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,20 +10,45 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
-            Документация к ботам ДИРМ 😉
-          </Link>
-        </div>
-      </div>
-    </header>
+<header className={clsx('container', styles.heroBanner)}>
+            <div className="row padding-horiz--md">
+                <div className="col col--7">
+                    <div className={clsx(styles.relative, 'row')}>
+                        <div className="col">
+                            <h1 className={styles.tagline}>
+                                Crawlee is a web<br /> scraping and browser<br /> automation library
+                            </h1>
+                            <h1 className={styles.tagline}>
+                                Crawlee is a <span>web<br /> scraping</span> and <span>browser<br /> automation</span> library
+                            </h1>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <h2>It helps you build reliable crawlers. Fast.</h2>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <div className={styles.heroButtons}>
+                                <Link to="docs/introduction" className={styles.getStarted}>Get Started</Link>
+                                <iframe src="https://ghbtns.com/github-btn.html?user=apify&repo=crawlee&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="170" height="30" title="GitHub"></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={clsx(styles.relative, 'col', 'col--5')}>
+                    <div className={styles.logoBlur}>
+                        <img src={require('../../static/img/logo-blur.png').default} className={clsx(styles.hideSmall)} />
+                    </div>
+                    <div className={styles.codeBlock}>
+                        <CodeBlock className="language-bash">
+                            npx crawlee create my-crawler
+                        </CodeBlock>
+                    </div>
+                </div>
+            </div>
+        </header>
   );
 }
 
